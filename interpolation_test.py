7f13996bfafb889a28a5e9f5d2e61a12f68fa7ba
oUJulienUo <julien.branlant@gmail.com>
2017-04-24 20:49:00 +0200

    Affichage graphe .dat + fonction spline + fonction splint

--- interpolation_test.py
+++ interpolation_test.py
@@ -2,3 +2,15 @@
 # coding: utf-8
 
 from interpolation import *
+from Open import *
+
+def display_dat():
+    (ex,ey,ix,iy) = load_foil("boe103.txt")
+    plt.plot([0, 1], [0, 0], color='black')
+    plt.plot(ex, ey, "b-o")
+    plt.plot(ix, iy, "b-o")
+    plt.title("Original points of boe103")
+    plt.show()
+
+
+display_dat()
