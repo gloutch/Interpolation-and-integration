d37e611a39aa3bbcf2caca1e68eb375cbe84b27f
oUJulienUo <julien.branlant@gmail.com>
2017-05-03 20:01:49 +0200

    maj

--- pressure_map.py
+++ pressure_map.py
@@ -6,41 +6,13 @@ import matplotlib.pyplot as plt
 import interpolation as int
 from airflow import *
 from Open import *
-from pressure_map import *
+from integration import *
 
-def create_pressure_map(file):
-    (ex, ey, ix, iy) = load_foil(file)
+# Links pressure with air speed
+def pressure_to_speed(flows_array, function):
     
-    hmin = np.min(iy)
-    hmax = np.max(ey)
+    pressure_speed = []
+    for i in range(0, len(flows_array)):
+        pressure_speed.append(length_of_plane_curves(function, flows_array[i][0], flows_array[i][1], len(flows_array), simpson))
     
-    new_intrados = int.apply_spline_lambda(ix, iy)
-    new_extrados = int.apply_spline_lambda(ex, ey)
-    
-    flow_extrados = create_pressures(hmax, new_extrados, 0.06, 500) # Third parameter changes accuracy
-    flow_intrados = create_pressures(hmin, new_intrados, 0.12, 500) # Third parameter changes accuracy
-    
-    for i in range(1, len(flow_extrados)):
-        plt.plot(flow_extrados[i][0], flow_extrados[i][1], "b")
-    for i in range (1, len(flow_intrados)):
-        plt.plot(flow_intrados[i][0], flow_intrados[i][1], "b")
-    
-    extrados = int.apply_spline(ex, ey, 200)
-    intrados = int.apply_spline(ix, iy, 200)
-    
-    plt.plot(extrados[0], extrados[1], "r")
-    plt.plot(intrados[0], intrados[1], "r")
-    
-    plt.title("Airflow map")
-    plt.show()
-
-    #######################
-    ## Add colored map ! ##
-    #######################
-
-print("#####################")
-print("#   Pressure map    #")
-print("#####################")
-print("===> Colored pressure map for boe103.dat <===")
-file = "boe103.txt"
-create_pressure_map(file)
+    return pressure_speed
