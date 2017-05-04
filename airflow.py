369435dc39680295ab467a7a6fa61ace81c13500
Elfen <xav.tps@hotmail.fr>
2017-05-04 12:27:53 +0100

    ariflow lambda

--- airflow.py
+++ airflow.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('C:\\Users\\Elfen\\Desktop\\Algonum5\\Interpolation-and-integration')
+
 ## Model airflow ##
 # coding: utf-8
 
@@ -28,4 +31,13 @@ def create_pressures(h, function, accuracy, nb_points):
         
         index +=1
     
+    return flows_array
+
+def create_pressures_lambda(h, function, accuracy, nb_points):
+    flows_array = []
+    index = 0
+    one_point = 1./nb_points
+    
+    for i in np.arange(0., 1., accuracy):
+        flows_array.append(flow_function(function, i, h))
     return flows_array
--- airflow_test.py
+++ airflow_test.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('C:\\Users\\Elfen\\Desktop\\Algonum5\\Interpolation-and-integration')
+
 ## Model airflow tests ##
 # coding: utf-8
 
@@ -18,7 +21,7 @@ def create_airflow_map(file):
     
     flow_extrados = create_pressures(hmax, new_extrados, 0.06, 500) # Third parameter changes accuracy
     flow_intrados = create_pressures(hmin, new_intrados, 0.12, 500) # Third parameter changes accuracy
-    
+    plt.figure(3)
     for i in range(1, len(flow_extrados)):
         plt.plot(flow_extrados[i][0], flow_extrados[i][1], "b")
     for i in range (1, len(flow_intrados)):
@@ -33,9 +36,49 @@ def create_airflow_map(file):
     plt.title("Airflow map")
     plt.show()
 
+def create_airflow_map_lambda(file):
+    (ex, ey, ix, iy) = load_foil(file)
+    
+    hmin = np.min(iy)
+    hmax = np.max(ey)
+    
+    new_intrados = int.apply_spline_lambda(ix, iy)
+    new_extrados = int.apply_spline_lambda(ex, ey)
+    
+    nbpoints = 500
+    
+    flow_extrados = create_pressures_lambda(hmax, new_extrados, 0.06, nbpoints) # Third parameter changes accuracy
+    flow_intrados = create_pressures_lambda(hmin, new_intrados, 0.12, nbpoints) # Third parameter changes accuracy
+    
+    plt.figure(4)
+    pas = (ex[len(ex)-1] - ex[0]) / nbpoints
+    x = np.arange(ex[0], ex[len(ex)-1], pas)
+    y = []
+    for i in range(1, len(flow_extrados)):
+        y = []
+        for j in range(nbpoints):
+            y.append(flow_extrados[i](x[j]))
+        plt.plot(x, y, "b")
+    for i in range (1, len(flow_intrados)):
+        y = []
+        for j in range(nbpoints):
+            y.append(flow_intrados[i](x[j]))
+        plt.plot(x, y, "b")
+    
+    extrados = int.apply_spline(ex, ey, 200)
+    intrados = int.apply_spline(ix, iy, 200)
+    
+    plt.plot(extrados[0], extrados[1], "r")
+    plt.plot(intrados[0], intrados[1], "r")
+    
+    plt.title("Airflow map")
+    plt.show()
+
 print("#####################")
 print("#   Airflow model   #")
 print("#####################")
 print("===> Laminar flowing for boe103.dat <===")
-file = "boe103.txt"
+#file = "boe103.txt"
+file = "C:\\Users\\Elfen\\Desktop\\Algonum5\\Interpolation-and-integration\\boe103.txt"
 create_airflow_map(file)
+create_airflow_map_lambda(file)
