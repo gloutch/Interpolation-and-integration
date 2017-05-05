4ab3c12fa483af9eb1797fda722440f737fbe876
bmellouki <mpsi.bilal@gmail.com>
2017-05-06 00:49:47 +0200

    Add files via upload
    a tester encore

---
+++ C2.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from scipy.misc import derivative
+
+def f(x):
+    return x**2
+
+
+def test_cont(f,a,b):
+    eps = 1e-20
+    X = np.linspace(a+eps,b-eps,(b-a)*100)
+    for x in X:
+        if ( f(x) != f(x+eps) ) or ( f(x) != f(x-eps) ) or ( f(x-eps) != f(x+eps) ):
+            return 0
+    return 1
+
+
+def test_deriv_1(f,a,b):
+    eps = 1e-17
+    X = np.linspace(a+eps,b-eps,(b-a)*100)
+    for x in X:
+        if (derivative(f,x-eps,0.01) != derivative(f,x+eps,0.01)) or (derivative(f,x,0.01) != derivative(f,x+eps,0.01)) or (derivative(f,x,0.01) != derivative(f,x-eps,0.01)):
+            return 0,x
+    return 1
+
+
+
+def test_deriv_2(f,a,b):
+    eps = 1e-17
+    X = np.linspace(a+eps,b-eps,(b-a)*100)
+    for x in X:
+        if (derivative(f,x-eps,0.01,2) != derivative(f,x+eps,0.01,2)) or (derivative(f,x,0.01,2) != derivative(f,x+eps,0.01,2)) or (derivative(f,x,0.01,2) != derivative(f,x-eps,0.01,2)):
+            return 0,x
+    return 1
+
+
+
+#il reste la continuite de la derivee
+
+def test_c_2(f,a,b):
+    if test_deriv_1(f,a,b) && test_deriv_2(f,a,b) && test_cont(f,a,b):
+        return 1
+    else:
+        return 0
