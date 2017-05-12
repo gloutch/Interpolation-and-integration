40020fce53903339755c349d9fa5647d66ee1104
LE FUR Gwendal <gwendal.lefur84@gmail.com>
2017-05-12 11:48:14 +0200

    ajout des tests gauss et romberg, gauss pas au point

--- integration.py
+++ integration.py
@@ -4,8 +4,8 @@
 from math import sqrt
 import numpy as np
 from interpolation import *
-from scipy.special import legendre
-from scipy.special import roots_legendre
+from numpy.polynomial import Legendre
+#from scipy.special import *
 
 # collection of method for approximation
 def trapeze(f, a, b):
@@ -18,17 +18,31 @@ def simpson(f, a, b):
     return f(a)/3. + 4 * f((a + b) / 2.)/3. + f(b)/3.
 
 def gauss(f, a=-1, b=1, eps=0.1):
-    gauss, s, r = eps, 0, 0
+    gauss, s, r = eps+1, 0, 0
     while (abs(gauss-s) > eps):
         gauss = s
         s = 0
         r += 1
-        roots = roots_legendre(r)
-        dPr = legendre(r, [a, b]).deriv()
+        Pr = Legendre(r, [a, b])
+        roots = Legendre.roots(Pr)
+        dPr = Pr.deriv()
         for i in range(len(roots)):
             w = 2./((1-roots[i]**2)*(dPr(roots[i])**2))
             s += w * f(roots[i])
     return s
+
+def romberg(f, a=-1, b=1, eps=1e-8):
+    tmp = [[0.5 * (b - a) * (f(a) + f(b))]] 
+    n = 1
+    while True:
+        h = float(b - a) / 2 ** n
+        tmp.append([None] * (n + 1))  
+        tmp[n][0] = 0.5*tmp[n-1][0] + h*sum(f(a+(2*k-1)*h) for k in range(1, 2**(n-1)+1))
+        for m in range(1, n+1):
+            tmp[n][m] = tmp[n][m-1] + (tmp[n][m-1] - tmp[n-1][m-1]) / (4 ** m - 1)
+        if abs(tmp[n][n-1] - tmp[n][n]) < eps:
+            return tmp[n][n]
+        n += 1
 
 # compute the integral of f on [a, b] using
 # nb_sub subdivision and with method approximation
